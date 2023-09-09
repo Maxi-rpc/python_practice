65663fa3786c66f7be01d5c9f1f1c7f4013a9bc6
Maximiliano <maxirpc2607@gmail.com>
2023-09-09 17:22:33 -0300

    ej4

--- Guada/23-08-22 matriz/ej1.py
+++ Guada/23-08-22 matriz/ej1.py
@@ -1,6 +1,6 @@
 ### Ej 1
 ## 5 equipos
-matriz = [                  # eq1 eq2 eq3 eq4 eq5
+goles = [                  # eq1 eq2 eq3 eq4 eq5
     [0, 3, 1, 2, 1], # eq 1
     [1, 0, 3, 2, 3], # eq 2
     [0, 2, 0, 1, 1], # eq 3
@@ -10,13 +10,6 @@ matriz = [                  # eq1 eq2 eq3 eq4 eq5
 
 #print(goles) # muestra todo
 
-# muestra cada equipo separado por ---
-count = 0
-for row in matriz:
-    print('fila', count)
-    print(row)
-    count +=1
-
 equipos = [
     { 'eq1': 0 },
     { 'eq2': 0 },
@@ -24,4 +17,19 @@ equipos = [
     { 'eq4': 0 },
     { 'eq5': 0 }
 ]
+
+# muestra cada equipo separado por ---
+cont = 0
+for fila in range(len(goles)):
+    #print('fila', fila) # muestra el num de fila
+    for col in range(len(goles[0])):
+        #print('col', col) # muestra num de columna
+        #print(goles[fila][col]) 
+        if(fila == cont and col == cont):
+            #print(cont)
+            goles[fila][col] = 1
+    cont += 1
+    
+
+
 
---
+++ Guada/23-08-22 matriz/ej4.py
@@ -0,0 +1,33 @@
+# crear tabla 5x15 
+from pprint import pprint
+marcoEj = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+marco = [
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+]
+
+filaStart = 0
+filaEnd = 4
+colStart = 0
+colEnd = 14
+for fila in range(5):
+    #print('fila', fila) # muestra el num de fila
+    for col in range(15):
+        #print('col', col) # muestra num de columna
+        #print(diagonal[fila][col]) 
+        if(fila == filaStart or fila == filaEnd or col == colStart or col == colEnd):
+            marco[fila][col] = 1 
+        else:
+            marco[fila][col] = 0
+
+pprint(marco)
