8b7d03711c62f2a4912d4ac33ba793984c9c17c3
Maximiliano <maxirpc2607@gmail.com>
2023-09-10 18:33:11 -0300

    Create practExamen-ej3.py

---
+++ Guada/23-08-22 matriz/practExamen-ej3.py
@@ -0,0 +1,46 @@
+# ej3
+def validarCar(palabra):
+    valid = True
+    if(len(palabra) > 5):
+        valid = False
+    return valid
+
+def buscarVoc(palabra):
+    vocales = ['A', 'E', 'I', 'O', 'U']
+    voc = []
+    cons = []
+    for letra in palabra:
+        if(letra in vocales):
+            voc.append(letra)
+        else:
+            cons.append(letra)
+    print(voc)
+    print(cons)
+
+    cont = len(cons)
+    palabraCon = ''
+    for i in range(len(palabra)):
+        print(palabraCon)
+
+
+
+
+def main():
+    print('ej 3')
+    palabra = ''
+
+    list_palabras = []
+    while(palabra != 'FINAL'):
+        palabra = input('Ingresar palabra: ')
+
+        while(validarCar(palabra) != True):
+            palabra = input('Ingresar palabra: ')
+        
+        list_palabras.append(palabra)
+        buscarVoc(palabra)
+    
+    
+
+
+main()
+
